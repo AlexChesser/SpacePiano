616767786d55bba33c35e7f15c026d8981b9bd25
Alex Chesser <achesser@klick.com>
2014-02-22 17:41:41 -0500

    sound production

--- js/SoundLayer.js
+++ js/SoundLayer.js
@@ -14,21 +14,33 @@ window.onload = function(){
 			*/
 			window.piano = MIDI;
 	        window.controller = new Leap.Controller();
-			window.currentframe = 0;
+			var currentframe = 0;
+			var notes = {};
+			
+			MIDI.setVolume(0, 255);
 			controller.on('frame', function(frame) {
 			  // your code here
 			  currentframe++
-		      if(frame.hands.length > 0 && currentframe % 60 == 0)
+		      if(frame.hands.length > 0 && currentframe % 10 == 0)
 		      {
+				  currentframe = 0;
 				  for (var i = 0; i < frame.hands.length; i++){
+
 			          var hand = frame.hands[i];
 			          var position = hand.palmPosition;
 			          var velocity = hand.palmVelocity;
 			          var direction = hand.direction;
-					  console.log(position);
-					  piano.noteOn(0, position, velocity, 0);
+					  var np = frame.interactionBox.normalizePoint(position, true);
+					  var  note = np[0];
+					  // the piano appears to start making noise at note #20 
+					  // i don't know how that maps on to anything but we want to normalize to a number 
+					  // between roughly 20 and ... more. 
+					  note = Math.round(note * 100+20,0);
+					  console.log(note);					  
+					  piano.noteOn(0, note, 512, 0);
 				  }
 			  }
+			  
 			});
 
 			window.controller.connect();
