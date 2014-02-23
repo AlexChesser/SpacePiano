424f89583f51b952a2ac0ac654c9d940c70fdb5b
Can Zhang <raitorm@gmail.com>
2014-02-22 19:37:47 -0500

    it actually plays now

--- js/KeysLayer.js
+++ js/KeysLayer.js
@@ -1,7 +1,30 @@
+
+function KeyTouch(isWhite, keyIndex, velocity) {
+        var _keyIndex = keyIndex;
+        var _isWhite = isWhite;
+        var _velocity = velocity;
+
+        return {
+            printKey: function () {
+                return "note: " + _keyIndex + "<br />" + "isWhite: " + _isWhite +
+            "<br />" + _velocity + "<br />";
+            },
+            setVelocity: function (velocity) {
+                _velocity = velocity;
+            },
+            getKeyIndex: function() {
+                return _keyIndex;
+            }
+        }
+    }
+
 var KeysLayer = {
 	WHITEKEY_NUMBER : 14,
     OVERALL_BORDER : 0.02,
-    KEY_WIDTH : (1 - 2 * this.OVERALL_BORDER) / this.WHITEKEY_NUMBER,
+    BASE_NOTE_INDEX: 40,
+    KEY_WIDTH : function() {
+            return (1 - 2 * this.OVERALL_BORDER) / this.WHITEKEY_NUMBER;
+        },
     KEYTOUCH_THRESHOLD : 0.05,
     getAllTouchedKeys: function(frame) {
         var resultList = [];
@@ -22,7 +45,7 @@ var KeysLayer = {
         return resultList;
     },
 	findKey : function(pos) {
-        var xIndex = Math.floor((pos[0] - this.OVERALL_BORDER) / this.KEY_WIDTH);
+        var xIndex = Math.floor((pos[0] - this.OVERALL_BORDER) / this.KEY_WIDTH());
         if (pos[2] > 0.5 && xIndex >= 0 && xIndex < this.WHITEKEY_NUMBER) {
             return new KeyTouch(true, xIndex, 0);
         }
@@ -32,37 +55,13 @@ var KeysLayer = {
             return new KeyTouch(false, xIndex, 0);
         }
     },
-    KeyTouch : function(isWhite, keyIndex, velocity) {
-        var _keyIndex = keyIndex;
-        var _isWhite = isWhite;
-        var _velocity = velocity;
-
-        return {
-            printKey: function () {
-                return "note: " + _keyIndex + "<br />" + "isWhite: " + _isWhite +
-            "<br />" + _velocity + "<br />";
-            },
-            setVelocity: function (velocity) {
-                _velocity = velocity;
-            }
-        }
-    },
 	PlayKeys: function(frame){
-	    var hands = frame.hands;
-	    var pointables = frame.pointables;
-
 	    var pointableOutput = document.getElementById("result");
 	    var pointableString = "";
 
 	    var keyList = this.getAllTouchedKeys(frame);
-
 	    for (var i = 0; i < keyList.length; i++) {
-	        pointableString += keyList[i].printKey();
-			//piano.noteOn(0, note, 127, 0);
-
-	
-	    }
-		
-		
+			piano.noteOn(0, this.BASE_NOTE_INDEX + keyList[i].getKeyIndex(), 127, 0);
+	    }	
 	}
 }
