4e7b3d703694989791c611e8d358bf825af8a15e
Alex Chesser <achesser@klick.com>
2014-02-22 23:27:34 -0500

    alterations

--- js/KeysLayer.js
+++ js/KeysLayer.js
@@ -1,65 +1,60 @@
-
-function KeyTouch(isWhite, keyIndex, velocity) {
-        var _keyIndex = keyIndex;
-        var _isWhite = isWhite;
-        var _velocity = velocity;
-
-        return {
-            printKey: function () {
-                return "note: " + _keyIndex + "<br />" + "isWhite: " + _isWhite +
-            "<br />" + _velocity + "<br />";
-            },
-            setVelocity: function (velocity) {
-                _velocity = velocity;
-            },
-            getKeyIndex: function() {
-                return _keyIndex;
-            },
-            getVelocity: function() {
-                return _velocity;
-            }
-        }
-    }
-
 var KeysLayer = {
-	WHITEKEY_NUMBER : 14,
-    OVERALL_BORDER : 0,
-    BASE_NOTE_INDEX: 50,
-    KEY_WIDTH : function() {
-            return (1 - 2 * this.OVERALL_BORDER) / this.WHITEKEY_NUMBER;
-        },
-    KEYTOUCH_THRESHOLD : 0.55,
-    getAllTouchedKeys: function(frame) {
-        var resultList = [];
-        var pointables = frame.pointables;
-        for (var i = 0, pointable; pointable = pointables[i++]; ) {
-            var interactionBox = frame.interactionBox;
-            var normalizedPosition = interactionBox.normalizePoint(pointable.tipPosition, true);
-            if (normalizedPosition[1] < 0.5 + this.KEYTOUCH_THRESHOLD &&
-                normalizedPosition[1] > 0.5 - this.KEYTOUCH_THRESHOLD) {
-                var key = this.findKey(normalizedPosition);
-                
-                if (key) {
-                    key.setVelocity(interactionBox.normalizePoint(pointable.tipVelocity, true)[1]);
-                    resultList.push(key);
-                }
-            }
-            
-            var mouseEvent = { clientX: $(window).width() * normalizedPosition[0], clientY: $(window).height() * normalizedPosition[2] };
-            onDocumentMouseMove(mouseEvent);
+	WHITEKEY_NUMBER : 108,
+	OVERALL_BORDER : 0,
+	BASE_NOTE_INDEX: 21,
+	KEY_WIDTH : function() {
+    	return (1 - 2 * this.OVERALL_BORDER) / this.WHITEKEY_NUMBER;
+	},
+	KEYTOUCH_THRESHOLD : 0.55,
+	getAllTouchedKeys: function(frame) {
+		var resultList = [];
+		var pointables = frame.pointables;
+		for (var i = 0, pointable; pointable = pointables[i++]; ) {
+			var interactionBox = frame.interactionBox;
+			var normalizedPosition = interactionBox.normalizePoint(pointable.tipPosition, true);
+			if (normalizedPosition[1] < 0.5 + this.KEYTOUCH_THRESHOLD &&
+				normalizedPosition[1] > 0.5 - this.KEYTOUCH_THRESHOLD) {
+					var key = this.findKey(normalizedPosition);
+					if (key) {
+						key.setVelocity(interactionBox.normalizePoint(pointable.tipVelocity, true)[1]);
+						resultList.push(key);
+					}
+				}
+				var mouseEvent = { clientX: $(window).width() * normalizedPosition[0], clientY: $(window).height() * normalizedPosition[2] };
+				onDocumentMouseMove(mouseEvent);
         }
-        
-        return resultList;
+		return resultList;
     },
-	findKey : function(pos) {
+	KeyTouch : function (isWhite, keyIndex, velocity) {
+	        var _keyIndex = keyIndex;
+	        var _isWhite = isWhite;
+	        var _velocity = velocity;
+
+	        return {
+	            printKey: function () {
+	                return "note: " + _keyIndex + "<br />" + "isWhite: " + _isWhite +
+	            "<br />" + _velocity + "<br />";
+	            },
+	            setVelocity: function (velocity) {
+	                _velocity = velocity;
+	            },
+	            getKeyIndex: function() {
+	                return _keyIndex;
+	            },
+	            getVelocity: function() {
+	                return _velocity;
+	            }
+	        }
+		},	
+		findKey : function(pos) {
         var xIndex = Math.floor((pos[0] - this.OVERALL_BORDER) / this.KEY_WIDTH());
         if (pos[2] > 0.5 && xIndex >= 0 && xIndex < this.WHITEKEY_NUMBER) {
-            return new KeyTouch(true, xIndex, 0);
+            return new this.KeyTouch(true, xIndex, 0);
         }
 
         if (pos[2] < 0.5 && xIndex >= 0 && xIndex < this.WHITEKEY_NUMBER
               && xIndex % 7 != 2 && xIndex % 7 != 6) {
-            return new KeyTouch(false, xIndex, 0);
+            return new this.KeyTouch(false, xIndex, 0);
         }
     },
 	PlayKeys: function(frame){
