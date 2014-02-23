f4ddcf518ed4a67e02dda712f03d63b909bb71c0
Alex Chesser <achesser@klick.com>
2014-02-23 12:27:56 -0500

    merged particles cleand up unused functions

--- js/KeysLayer.js
+++ js/KeysLayer.js
@@ -1,75 +1,4 @@
 var KeysLayer = {
-	WHITEKEY_NUMBER : 108,
-    OVERALL_BORDER : 0,
-    BASE_NOTE_INDEX: 21,
-    KEY_WIDTH : function() {
-            return (1 - 2 * this.OVERALL_BORDER) / this.WHITEKEY_NUMBER;
-        },
-    KEYTOUCH_THRESHOLD : 0.2,
-    getAllTouchedKeys: function(frame) {
-        var resultList = [];
-        var pointables = frame.pointables;
-        for (var i = 0, pointable; pointable = pointables[i++]; ) {
-			
-            var interactionBox = frame.interactionBox;
-            var normalizedPosition = interactionBox.normalizePoint(pointable.tipPosition, true);
-            var key = this.findKey(normalizedPosition);
-                
-            if (key) {
-                resultList.push(key);
-            }
-            
-            var mouseEvent = { clientX: $(window).width() * normalizedPosition[0], clientY: $(window).height() * normalizedPosition[2] };
-            onDocumentMouseMove(mouseEvent);
-        }
-        
-        return resultList;
-    },
-	KeyTouch: function(isWhite, keyIndex, height) {
-	        var _keyIndex = keyIndex;
-	        var _isWhite = isWhite;
-	        var _height = height;
-
-	        return {
-	            printKey: function () {
-	                return "note: " + _keyIndex + "<br />" + "isWhite: " + _isWhite +
-	            "<br />" + _height + "<br />";
-	            },
-	            getKeyIndex: function() {
-	                return _keyIndex;
-	            },
-	            getHeight: function() {
-	                return _height;
-	            },
-	            isWhite: function() {
-	                return _isWhite;
-	            }
-	        }
-	    },	
-	findKey : function(pos) {
-        var xIndex = Math.floor((pos[0] - this.OVERALL_BORDER) / this.KEY_WIDTH());
-        if (pos[2] > 0.5 && xIndex >= 0 && xIndex < this.WHITEKEY_NUMBER) {
-            return new this.KeyTouch(true, xIndex, pos[1]);
-        }
-
-        if (pos[2] < 0.5 && xIndex >= 0 && xIndex < this.WHITEKEY_NUMBER
-              && xIndex % 7 != 2 && xIndex % 7 != 6) {
-            return new this.KeyTouch(false, xIndex, pos[1]);
-        }
-    },
-    numberOfSkippedBlackKeys : function(index) {
-       return Math.floor(index / 7) + Math.floor((index % 7) / 3);
-    },
-    whiteKeyIndexToNote : function(index) {
-        return this.BASE_NOTE_INDEX + 2 * index - this.numberOfSkippedBlackKeys(index);
-    },
-    blackKeyIndexToNote : function(index) {
-        return this.BASE_NOTE_INDEX + 2 * index + 1 - this.numberOfSkippedBlackKeys(index);  
-    },
-	tmp: function(frame){
-		console.log(this.getAllTouchedKeys(frame));
-		this.tmp = function(){};
-	},
 	//  Left/Right = pos[0] Min = -400 Max = 400
 	//		notes #start at 21 go to 108
 	//  Up/Down = pos[1] Min = ~100 Max = ~500
@@ -104,8 +33,6 @@ var KeysLayer = {
 			piano.noteOn(i, note, velocity, 0);
 			this.reDraw3d(frame.interactionBox.normalizePoint(position, true))
 		}
-	
-
 	},
 	SimplePlay: function(frame){
 		// replacement for playkeys which will play notes based just on the position of the hand.
@@ -130,25 +57,5 @@ var KeysLayer = {
 		for(var i = 0; i < frame.hands.length; i++) {
 			this.SimplePlayLoop(frame, frame.pointables, "tipPosition");
 		}
-	},
-	PlayKeys: function(frame){
-	    var pointableOutput = document.getElementById("result");
-	    var pointableString = "";
-
-	    var keyList = this.getAllTouchedKeys(frame);
-		
-	    for (var i = 0; i < keyList.length; i++) {
-			
-			//piano.noteOn(0, 21+(Math.round(keyList[i].getKeyIndex()*100)), (1 - keyList[i].getHeight()) * 127, 0);
-			console.log(keyList[i].getKeyIndex());
-			
-			
-            if (keyList[i].isWhite()) {
-			    piano.noteOn(0, this.whiteKeyIndexToNote(keyList[i].getKeyIndex()), (1 - keyList[i].getHeight()) * 127, 0);
-            } else {
-                piano.noteOn(0, this.blackKeyIndexToNote(keyList[i].getKeyIndex()), (1 - keyList[i].getHeight()) * 127, 0);
-            }
-
-	    }	
 	}
 }
